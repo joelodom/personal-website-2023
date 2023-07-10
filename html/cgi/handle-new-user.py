77042fd645b9191b3e5645fe1d355e46c5a6468c
Joel Odom <joelodom@gmail.com>
2023-07-10 23:09:40 +0000

    Added salt

--- html/cgi/handle-new-user.py
+++ html/cgi/handle-new-user.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import cgi
+import hashlib
+import secrets
 
 # Create instance of FieldStorage
 form = cgi.FieldStorage()
@@ -16,4 +18,13 @@ print("Content-type: text/html\n")
 print("<h2>Received Data:</h2>")
 print("<p>Email: {}</p>".format(email))
 print("<p>Password: {}</p>".format(password))
+
+
+password = password.encode("utf-8")  # Convert the password to bytes
+salt = secrets.token_bytes(16)
+
+key = hashlib.pbkdf2_hmac("sha256", password, salt, iterations=310000)
+
+# The derived key can be used for encryption, authentication, or other purposes
+print(f"<p>Key: {key.hex()}</p>")
 
