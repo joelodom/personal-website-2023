996562a8f5c3759c866e9385e1b1caa727292de6
Joel Odom <joelodom@gmail.com>
2023-12-17 17:50:26 -0500

    I'm now pulling my MongoDB Atlas API key from Secrets Manager, but not doing anything with it quite yet.

--- mongodb-via-aws/receive-sensor-data.py
+++ mongodb-via-aws/receive-sensor-data.py
@@ -1,8 +1,8 @@
 import json
 import boto3
-import os
 import logging
 import base64
+from botocore.exceptions import ClientError
 
 #
 # TODO: Don't use the same bucket as the website content
@@ -11,11 +11,35 @@ import base64
 
 BUCKET = 'joelodom'
 FOLDER = 'received-data'
+MONGO_DB_API_KEY = 'mongo-joel-api-key2'
+MONGO_DB_API_KEY_REGION = 'us-east-1'
+KEY_NAME = 'mongodb-api-key'
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 s3 = boto3.client('s3')
+
+# Create a Secrets Manager client
+session = boto3.session.Session()
+client = session.client(
+    service_name = 'secretsmanager',
+    region_name = MONGO_DB_API_KEY_REGION
+)
+
+mongo_api_key = None
+
+try:
+    mongo_api_key = client.get_secret_value(
+        SecretId = MONGO_DB_API_KEY
+    )
+    mongo_api_key = mongo_api_key['SecretString'] # kind of a hack
+    mongo_api_key = json.loads(mongo_api_key) # even worse
+    mongo_api_key = mongo_api_key[KEY_NAME] # inexcusable
+except ClientError as e:
+    # For a list of exceptions thrown, see
+    # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+    raise e
 
 def encode_to_base64(input_string):
     byte_data = input_string.encode('utf-8')
